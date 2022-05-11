5f8ad93297ce7c50b782b7f85b47c5a8cd610c09
Bolu <boluadetifa@gmail.com>
2022-05-11 18:28:11 +0100

    make it better

--- src/app.ts
+++ src/app.ts
@@ -24,10 +24,10 @@ app.use(helmet())
 app.use(morgan('dev'))
 
 const pool = mysql.createPool({ 
-    host : 'localhost',
-    user : 'guest', 
-    password : '1234',
-    database : 'esusu'
+    host : 'us-cdbr-east-05.cleardb.net',
+    user : 'b5f8bfe1de3b3f', 
+    password : 'fb26353d',
+    database : 'heroku_4d7a7ecebc52925'
 })
 
 pool.query('select 1 + 1', (err:any, rows:any) => { /* */ });
--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 
 // create connection
 const pool = mysql.createPool({ 
-    host : 'localhost',
-    user : 'guest', 
-    password : '1234',
-    database : 'esusu'
+    host : 'us-cdbr-east-05.cleardb.net',
+    user : 'b5f8bfe1de3b3f', 
+    password : 'fb26353d',
+    database : 'heroku_4d7a7ecebc52925'
 })
 
 module.exports = pool
